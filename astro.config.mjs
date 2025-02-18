98b6227a9bd0e593a4368d4181f5cc56f21cc4f8
Apps Factory <appsfactorycolombia@gmail.com>
2025-02-17 22:19:43 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
+import awsAmplify from 'astro-aws-amplify';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'static',
+  adapter: awsAmplify(),
+  output: 'server',
 });
